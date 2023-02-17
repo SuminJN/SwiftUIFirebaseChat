b2197e8d00f8927ab1f4d53ae7bfe5dfb1e6d5cf
SuminJN <wjstnals1211@handong.ac.kr>
2023-02-17 13:51:43 +0900

    Initial Commit

---
+++ SwiftUIFirebaseChat/SwiftUIFirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseChatApp.swift
+//  SwiftUIFirebaseChat
+//
+//  Created by 전수민 on 2023/02/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
